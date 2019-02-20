3672a545ea8439577ead14192d71c7b9bd048336
Kamil Nowinski <kamil.nowinski@asos.com>
2019-02-20 07:45:32 +0000

    Init commit

---
+++ CRM/dbo/Stored Procedures/RunPostProcessingJob.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[RunPostProcessingJob]
+AS
+BEGIN
+
+	--Run 'CrmPostProcessing' SQL Job:
+	EXEC msdb.dbo.sp_start_job @job_name = N'CrmPostProcessing';
+ 
+	RETURN 0
+END
---
+++ CRM/dbo/Views/vCustomer.sql
@@ -0,0 +1,4 @@
+﻿CREATE VIEW [dbo].[vAuditCustomer]
+AS 
+SELECT [auditCustomerId], [auditAction], [CustomerId], [RegionCode], [Title], [SSN], [Login], [CountryCode], [FirstName], [Surname], [isActive], [InactiveDate], [CreatedOn], [CreatedBy], [UpdatedOn], [UpdatedBy], [CustomerTypeCode], [Twitter] 
+FROM [$(SecondDatabase)].[audit].[Customer];
