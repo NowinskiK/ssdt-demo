fc2aedf920efd9ebac7aa460c39a10b29a9812ed
Kamil Nowinski <kamil.nowinski@asos.com>
2019-02-26 18:55:51 +0000

    Added user + read-only access permission

---
+++ SecondDatabase/Deployment/CreateUsers.sql
@@ -0,0 +1,11 @@
+﻿USE [$(DatabaseName)]
+GO
+
+IF NOT EXISTS (SELECT 1 FROM sys.database_principals dp WHERE dp.name = '$(SvcWebSite)')
+BEGIN
+    PRINT 'User [$(SvcWebSite)] created in [$(DatabaseName)] database.';
+    CREATE USER [$(SvcWebSite)] FROM LOGIN [$(SvcWebSite)];
+END
+EXEC sys.sp_addrolemember @rolename = 'db_datareader', @membername = [$(SvcWebSite)];
+PRINT 'User [$(SvcWebSite)] was granted db_datareader role permissions in [$(DatabaseName)] database.';
+GO
